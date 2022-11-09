fb7f434f34cb9a71e180cd0a1972d1b9fe9dd237
padmanath <117708308+padmanath@users.noreply.github.com>
2022-11-09 13:12:52 +0530

    Add files via upload

---
+++ Resturent.java
@@ -0,0 +1,71 @@
+package com.ty.Resturent.project;
+
+import java.util.Scanner;
+
+public class Resturent {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		String[] s = {"pizza    ","burger    ",};
+		int[] rate = new int[]{30,35};
+		int[] qt = new int[rate.length];
+	        int sum=0;
+	        
+		    boolean quit= false ;
+	         
+	        
+	        do{
+	            System.out.println("ITEM"+"\t\t\tPrice");
+	            
+	       
+	            
+			for(int i=0;i<rate.length;i++) {
+				
+	            System.out.println((i+1)+"."+s[i]+"\t\t"+rate[i]);
+			}
+	          
+	            
+	            int ch=input.nextInt();
+	            
+		    if(ch>0 && ch<2)
+		    {
+			System.out.println("enter the no of quantites of "+s[ch-1]);
+			
+	                 int q=input.nextInt();
+	                 
+		         qt[ch-1]+=q;
+			
+		    }
+		    else
+		    {
+		    	
+	               quit=false;
+	               System.out.println("sorry please reselect the iteam again .......thank you ");
+	              
+	            }
+	        
+	        System.out.println("Your Orders are:\n");
+	        
+		    for(int i=0;i<rate.length;i++)
+		    {
+	             if(qt[i]!=0)
+		     {
+		      sum+=qt[i]*rate[i];
+	              System.out.println(s[i]+"*   "+qt[i]+"=="+qt[i]*rate[i]+"rs");
+		     }
+		    }
+	  
+	       
+	        System.out.println("Your total bill="+sum);
+	        
+	         System.out.println("Thank you");
+	        }while(quit); 
+	        
+
+	}
+
+}
