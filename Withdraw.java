4ec21095adce494812b7736736ba5349c8349732
padmanath <117708308+padmanath@users.noreply.github.com>
2022-11-10 10:27:01 +0530

    Add files via upload

---
+++ Withdraw.java
@@ -0,0 +1,23 @@
+package BankAccount;
+
+public class Withdraw {
+	
+	     public static void main(String[] args) {
+	 
+ 
+		    try {
+		      int[] myNumbers = {1, 2, 3};
+		      System.out.println(myNumbers[10]);
+		    }
+		    
+		    catch (Exception e) {
+		      System.out.println("Something goes wrong.");
+		    } finally {
+		      System.out.println("your no is 10");
+		    }
+		    
+		  }
+}
+
+       
+
